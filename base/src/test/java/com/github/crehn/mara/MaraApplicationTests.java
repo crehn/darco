fbfb8f0c8ece95a51ea80a92666d81add1a1e97e
Christian Rehn <mail@christian-rehn.de>
2019-12-29 18:45:13 +0100

    added mara-base

---
+++ base/src/test/java/com/github/crehn/mara/MaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.crehn.mara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
